842b1009d8fc2ef36a0142d9af21a998ff04df14
DanielleKerr17 <47329837+DanielleKerr17@users.noreply.github.com>
2019-02-07 11:01:46 -0600

    Demo 1 feb 7

--- 01/sketch.js
+++ 01/sketch.js
@@ -8,8 +8,15 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
+  let x = 0;
+  let xSpeed = 6;
 }
 
 function draw() {
-  background(220);
+  background(255);
+  fill(255, 204, 0);
+  ellipse(200,200 125);
+  fill(29,143,178);
+  rect(mouseX, 400, 75,75);
 }
