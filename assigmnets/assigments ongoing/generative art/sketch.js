21947bac8db8d77a106178e0bce3954db2d2efe7
Kerr1601379 <1601379@sps-student.sk.ca>
2019-04-29 10:15:44 -0600

    04/29/19

---
+++ assigmnets/assigments ongoing/generative art/sketch.js
@@ -0,0 +1,69 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+//-------------------------------------------------------------------------------------------------------
+
+function setup() {
+ //createCanvas(4500, 3000);
+ createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
+  noLoop();
+}
+
+function draw() {
+  background(0);
+  triangle1();
+  triangle2();
+    
+}
+
+function keyPressed(){
+  if (key === ""){
+    save(); 
+  }
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+
+function triangle1(){
+  rectMode(CENTER);
+  translate(width/2,height *0.8);
+  for(let i = 0; i < 15; i ++){
+    
+      push();
+      translate(0, i * -50);
+      rotate((i*10));
+      fill(225, 0, 100 , map(i, 0, 10, 70, 0 ));    //changes what we can do with the for loop and variable
+      stroke(170);
+      strokeWeight(0.5);
+      triangle(-100, 100, 0, -100, 100, 100);
+      pop();
+
+    
+  }
+  pop();
+}
+
+function triangle2(){
+  rectMode(CENTER);
+  translate(width/2,height *0.8);
+  for(let i = 0; i < 15; i ++){
+    
+      push();
+      translate(0, i * -50);
+      rotate((i*10));
+      fill(225, 0, 100 , map(i, 0, 10, 70, 0 ));    //changes what we can do with the for loop and variable
+      stroke(170);
+      strokeWeight(0.5);
+      triangle(-100, 100, 0, -100, 100, 100);
+      pop();
+
+    
+  }
+  
+}
+
+
--- assigmnets/assigments ongoing/refactoring 4/sketch.js
+++ assigmnets/assigments ongoing/refactoring 4/sketch.js
@@ -3,290 +3,39 @@
 // 04/04/19
 //
 let fillColor = 0; 
-let fillColorOpp = 0;
 
 function setup() {
   createCanvas(600, 600);
  }
 
+ //----------------------------------------------------------------------------------------------
 
   function draw() {
   drawCheckers();
   
   }
 
-
+//-------------------------------------------------------------------------------------
 
 function drawCheckers(){
 let columnY = 0; 
-  for (let x = 0; x< 526; x += 75){
-    for (let y = 0; y < 526; y += 75){
-      //let y = columnY;
-      rect(x,y,75,75); 
-      fill(fillColor);
+  for (let x = 0; x< 526; x += 75){           //rows
+    for (let y = 0; y < 526; y += 75){        //columns
       if (fillColor === 0){
         fillColor = 255;
       }
       else{
         fillColor = 0;
       }
+      fill(fillColor);
+      rect(x,y,75,75);                //draws rectangles
     }
-    fillColor = 255;
-    fill(fillColorOpp);
+    if (fillColor === 0){             //one more before the loop to resart the columns 
+      fillColor = 255;
+    }
+    else{
+      fillColor = 0;
+    }
+
   }
-
-
-  // rect(COLUMN_1,columnY,75,75);
-  // fill(BLACK_S);
-
-
-  // rect(COLUMN_1,columnY,75,75);
-  // fill(WHITE_S);
-
-
-  // rect(COLUMN_1,columnY,75,75);
-  // fill(BLACK_S);
-
-
-  // rect(COLUMN_1,columnY,75,75);
-  // fill(WHITE_S);
-
-
-  //  rect(COLUMN_1,columnY,75,75);
-  //  fill(BLACK_S);
-
-
-  //  rect(COLUMN_1,columnY,75,75);
-  //  fill(WHITE_S);
-
-  //  rect(COLUMN_1,columnY,75,75);
-  //  fill(BLACK_S);
-
-  
-  // rect(COLUMN_1,columnY,75,75);
-  // fill(BLACK_S);
-
-  // colum 2
-  rect(75,0,75,75);
-  fill(WHITE_S);
-
-
-  rect(75,75,75,75);
-  fill(BLACK_S);
-
-
-  rect(75,150,75,75);
-  fill(WHITE_S);
-
-
-  rect(75,225,75,75);
-  fill(BLACK_S);
-
-
-  rect(75,300,75,75);
-  fill(WHITE_S);
-
-
-  rect(75,375,75,75);
-  fill(BLACK_S);
-
-
-  rect(75,450,75,75);
-  fill(WHITE_S);
-
-
-  rect(75,525,75,75);
-  fill(WHITE_S);
-
-  //3
-
-  rect(150,0,75,75);
-  fill(BLACK_S);
-
-  rect(150,75,75,75);
-  fill(WHITE_S);
-  
-  rect(150,150,75,75);
-  fill(BLACK_S);
-
-
-  rect(150,225,75,75);
-  fill(WHITE_S);
-
-
-  rect(150,300,75,75);
-  fill(BLACK_S);
-
-
-  rect(150,375,75,75);
-  fill(WHITE_S);
-
-
-  rect(150,450,75,75);
-  fill(BLACK_S);
-
-
-  rect(150,525,75,75);
-  fill(BLACK_S);
-
-  //4
-
-  rect(225,0,75,75);
-  fill(WHITE_S);
-
-
-  rect(225,75,75,75);
-  fill(BLACK_S);
-
-
-  rect(225,150,75,75);
-  fill(WHITE_S);
-
-
-  rect(225,225,75,75);
-  fill(BLACK_S);
-
-
-  rect(225,300,75,75);
-  fill(WHITE_S);
-
-
-  rect(225,375,75,75);
-  fill(BLACK_S);
-
-  
-  rect(225,450,75,75);
-  fill(WHITE_S);
-
-
-  
-  rect(225,525,75,75);
-  fill(WHITE_S);
-
-  //5 
-  rect(300,0,75,75);
-  fill(BLACK_S);
-
-
-  rect(300,75,75,75);
-  fill(WHITE_S);
-
-
-  rect(300,150,75,75);
-  fill(BLACK_S);
-
-
-  rect(300,225,75,75);
-  fill(WHITE_S);
-
-
-  rect(300,300,75,75);
-  fill(BLACK_S);
-
-
-  rect(300,375,75,75);
-  fill(WHITE_S);
-
-
-  rect(300,450,75,75);
-  fill(BLACK_S);
-
- 
-  rect(300,525,75,75);
-  fill(BLACK_S);
- 
-  //6
-
-  rect(375,0,75,75);
-  fill(WHITE_S);
-
-
-  rect(375,75,75,75);
-  fill(BLACK_S);
-
-
-  rect(375,150,75,75);
-  fill(WHITE_S);
-
-
-  rect(375,225,75,75);
-  fill(BLACK_S);
-
-
-  rect(375,300,75,75);
-  fill(WHITE_S);
-
-
-  rect(375,375,75,75);
-  fill(BLACK_S);
-
-
-  rect(375,450,75,75);
-  fill(WHITE_S);
-
-
-  rect(375,525,75,75);
-  fill(WHITE_S);
-
-  //7
-
-  rect(450,0,75,75);
-  fill(BLACK_S);
-
-
-  rect(450,75,75,75);
-  fill(WHITE_S);
-
-
-  rect(450,150,75,75);
-  fill(BLACK_S);
-
-
-  rect(450,225,75,75);
-  fill(WHITE_S);
-
-
-  rect(450,300,75,75);
-  fill(BLACK_S);
-
-
-  rect(450,375,75,75);
-  fill(WHITE_S);
-
-
-  rect(450,450,75,75);
-  fill(BLACK_S);
-
-  rect(450,525,75,75);
-  fill(BLACK_S);
-
-// 8
-
-  rect(525,0,75,75);
-  fill(WHITE_S);
-
-
-  rect(525,75,75,75);
-  fill(BLACK_S);
-
-
-  rect(525,150,75,75);
-  fill(WHITE_S);
-
-
-  rect(525,225,75,75);
-  fill(BLACK_S);
-
-
-  rect(525,300,75,75);
-  fill(WHITE_S);
-
-
-  rect(525,375,75,75);
-  fill(BLACK_S);
-
-
-  rect(525,450,75,75);
-  fill(WHITE_S);
-
 }
